b3aad59a10e034431c7b862ce5ae194c24d87330
Mani Wadhwa <maniwadhwa26@gmail.com>
2019-01-13 00:16:38 +0530

    Added Orders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
-
-
+import Orders from './containers/Orders/Orders';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+
 class App extends Component {
   render() {
     return (
@@ -12,6 +12,7 @@ class App extends Component {
         <Layout>
           <Switch>
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
           <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
