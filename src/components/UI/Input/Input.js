1434fd7e87b6829779f5aae56dada2f1733e3c80
Mani Wadhwa <maniwadhwa26@gmail.com>
2019-01-16 00:01:25 +0530

    FormValidation Done

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,42 +1,51 @@
 import React from 'react';
 import classes from './Input.css';
-const input =(props) => {
+const input = (props) => {
 
 
     let inputElement = null;
 
-
-    switch(props.elementType){
+    const inputClasses = [classes.InputElement];
+    if(props.invalid && props.shouldValidate
+        && props.touched
+    ){
+        inputClasses.push(classes.Invalid);
+    }
+    switch (props.elementType) {
         case ('input'):
-            inputElement = <input onChange={props.changed} className={classes.Input} {...props.elementConfig} value={props.value}/>;
+            inputElement = <input 
+            onChange={props.changed} 
+            className={inputClasses.join(' ')} 
+            {...props.elementConfig} 
+            value={props.value} />;
             break;
         case ('textarea'):
             inputElement = <textarea onChange={props.changed} className={classes.Input} {...props.elementConfig} value={props.value} />;
             break;
         case ('select'):
-            inputElement = 
-            <select 
-            onChange={props.changed}
-                className={classes.Input}
-                value={props.value} 
+            inputElement =
+                <select
+                    onChange={props.changed}
+                    className={classes.Input}
+                    value={props.value}
 
-            >
-                {props.elementConfig.options.map(optionOb => (
+                >
+                    {props.elementConfig.options.map(optionOb => (
                         <option key={optionOb.value} value={optionOb.value}>{optionOb.displayValue}</option>
-                ))}
+                    ))}
 
-            </select>;
+                </select>;
             break;
         default:
             inputElement = <input onChange={props.changed} className={classes.Input} {...props.elementConfig} value={props.value} />;
             break;
     }
-    
-    return(
-    <div className={classes.Input}>
-        <label className={classes.Label}>{props.lable}</label>
-        {inputElement}
-    </div>
+
+    return (
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.lable}</label>
+            {inputElement}
+        </div>
     );
 
 };
