41b58deba0c29f4ff0dd1ecda21945e514790827
Mani Wadhwa <maniwadhwa26@gmail.com>
2019-02-28 20:10:27 +0530

    Done

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,4 +1,4 @@
 export { addIngredient,removeIngredient,initIngredients,setIngredients,fetchIngredientsFailed } from './burgerBuilder';
-export { purchaseBurger,purchaseInit,fetchOrders} from './order';
+export { purchaseBurger,purchaseInit,fetchOrders, purchaseBurgerStart, purchaseBurgerSuccess,purchaseBurgerFail,fetchOrdersSuccess,fetchOrdersFail,fetchOrdersStart} from './order';
 export {auth,logout,setAuthRedirectPath,authCheckState,logoutSucceed,authStart,authSuccess,checkAuthTimeout,authFail} from './auth';
 
