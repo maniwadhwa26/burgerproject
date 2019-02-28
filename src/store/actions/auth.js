7d325b9ae8f4f2cda97b928eb11e1d54072e6574
Mani Wadhwa <maniwadhwa26@gmail.com>
2019-02-28 17:59:41 +0530

    Redux-Saga more

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,4 @@
 import * as actionTypes from './actionTypes';
-import axios from 'axios';
 export const authStart = () => {
     return {
         type: actionTypes.AUTH_START
@@ -27,42 +26,26 @@ export const logout = () => {
         type: actionTypes.AUTH_INITIATE_LOGOUT
     };
 };
+export const logoutSucceed = () =>{
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, expirationTime * 1000);
-    }
-}
+   return {
+    type: actionTypes.AUTH_CHECK_TIMEOUT,
+    expirationTime: expirationTime
+   };
+};
 
 
 export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        //..
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToke: true
-        };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAKRTqp6nzG7REROETIqeRPSIvtgSlzV_g';
-        if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAKRTqp6nzG7REROETIqeRPSIvtgSlzV_g';
-        }
-        axios.post(url, authData)
-            .then(response => {
-                console.log(response);
-                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-                localStorage.setItem('token', response.data.idToken);
-                localStorage.setItem('expirationDate',expirationDate);
-                localStorage.setItem('userId',response.data.localId);
-                dispatch(authSuccess(response.data.idToken, response.data.localId));
-                dispatch(checkAuthTimeout(response.data.expiresIn));
-            })
-            .catch(err => {
-                dispatch(authFail(err.response.data.error));
-            })
-    };
+    return {
+        type: actionTypes.AUTH_USER,
+        email:email,
+        password:password,
+        isSignup : isSignup
+    }
 };
 
 export const setAuthRedirectPath = (path) => {
@@ -73,23 +56,7 @@ export const setAuthRedirectPath = (path) => {
 };
 
 export const authCheckState = () => {
-    return dispatch => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            dispatch(logout());
-        }
-        else {
-            const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            if (expirationDate <= new Date()) {
-                dispatch(logout());
-                
-            }
-            else {
-                const userId = localStorage.getItem('userId');
-                dispatch(authSuccess(token,userId));
-                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime())) / 1000 );
-            }
-
-        }
-    };
+    return {
+        type: actionTypes.AUTH_CHECK_STATE
+    }
 };
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,4 @@
-export { addIngredient,removeIngredient,initIngredients } from './burgerBuilder';
+export { addIngredient,removeIngredient,initIngredients,setIngredients,fetchIngredientsFailed } from './burgerBuilder';
 export { purchaseBurger,purchaseInit,fetchOrders} from './order';
-export {auth,logout,setAuthRedirectPath,authCheckState} from './auth';
+export {auth,logout,setAuthRedirectPath,authCheckState,logoutSucceed,authStart,authSuccess,checkAuthTimeout,authFail} from './auth';
+
